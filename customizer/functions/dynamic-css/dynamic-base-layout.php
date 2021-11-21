eb4987cee7053e3bab8bf55a0b7e81d8a506b2b8
Anuj Subedi <struggleage@gmail.com>
2021-11-21 13:38:19 +0545

    Design works
    Design works

--- customizer/functions/dynamic-css/dynamic-base-layout.php
+++ customizer/functions/dynamic-css/dynamic-base-layout.php
@@ -13,8 +13,8 @@ if ( ! function_exists( 'xenial_dynamic_layout_css' ) ) {
 			--xe_container_width: ' . absint( $contentAreaWidth ) . 'px;
 			--xe_default_primary_area_width: calc( 100% - ' . absint( $sidebarWidth ) . '% );
 			--xe_default_sidebar_width: ' . absint( $sidebarWidth ) .  '%;
-			--xe_narrow_section_base_width: ' . absint( $narrowcontentAreaWidth ) . 'px;
-			--xe_default_boxed_layout_container_maxwidth: ' . absint( $siteContainerWidth ) . 'px;
+			--xe_container_width_narrow: ' . absint( $narrowcontentAreaWidth ) . 'px;
+			--xe_container_width: ' . absint( $siteContainerWidth ) . 'px;
 		}';
 
 		return $css;
