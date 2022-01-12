ed7a7fae98b3a7e1bd924f5b8d4f5e71f24940c3
h1dd3n_sn1p3r <struggleage@gmail.com>
2022-01-12 13:29:19 +0545

    Added: EDD MiniCart Style
    Added: EDD MiniCart Style

--- template-parts/theme-header/elements/element-edd-minicart.php
+++ template-parts/theme-header/elements/element-edd-minicart.php
@@ -18,7 +18,7 @@
                     </div>
                     <div class="action">
                         <button class="xe-button xe-false-button edd-remove-from-cart">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10-4.477 10-10 10zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm0-9.414l2.828-2.829 1.415 1.415L13.414 12l2.829 2.828-1.415 1.415L12 13.414l-2.828 2.829-1.415-1.415L10.586 12 7.757 9.172l1.415-1.415L12 10.586z" fill="rgba(130,130,130,1)"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg>
                         </button>
                     </div>
                 </li>
