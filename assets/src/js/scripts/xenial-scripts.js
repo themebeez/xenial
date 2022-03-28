5a374bce358ef6684d954d58df84f4b634a38389
h1dd3n_sn1p3r <struggleage@gmail.com>
2022-03-28 13:03:36 +0545

    Added: Addonify wishlist icon support for tippy
    Added: Addonify wishlist icon support for tippy

--- assets/src/js/scripts/xenial-scripts.js
+++ assets/src/js/scripts/xenial-scripts.js
@@ -9,285 +9,193 @@
 ===============================================================================================
 */
 
-document.addEventListener("DOMContentLoaded", function() {
+/**
+ * 
+ * Tooltip function.
+ * 
+*/
 
-    // Init functions
-    var xeInitAll = function() {
+function xeToolTip() {
 
-        xeToolTip();
-        xeToggleComment();
-        xeBackToTopButton();
-        xeCanvasNavigationSubMenuToggleBtn();
-        //xeTabs();
-        //xeTabHiddenItems();
-        //initSwiper();
-    }
+    tippy(".xe-tippy", {
+        content: "",
+        allowHTML: false,
+        arrow: true,
+        delay: 10,
+        duration: 350,
+        maxWidth: 350,
+        placement: "left",
+        animation: "scale",
+        onTrigger: function (instance) {
 
-    xeInitAll();
+            // Support for Addonify wishlist.
 
-    // Tooltip
-    function xeToolTip() {
-        tippy(".xe-tippy", {
-            content: "",
-            allowHTML: false,
-            arrow: true,
-            delay: 10,
-            duration: 350,
-            maxWidth: 350,
-            placement: "left",
-            animation: "scale",
+            if (instance.reference.classList.contains('added-to-wishlist')) {
+
+                // get the 'data-tippy-content' attribute of the element.
+                var tooltipContent = instance.reference.getAttribute('data-tippy-content');
+                // change the tooltip content.
+                instance.setContent(tooltipContent);
+            }
+        },
+    });
+}
+
+
+/**
+ * 
+ * Comment function.
+ * 
+*/
+
+function xeToggleComment() {
+
+    var body = document.body;
+    var commentToggleBtn = document.getElementById("xe-comment-toggle-btn");
+
+    if (commentToggleBtn !== null || undefined) {
+
+        commentToggleBtn.addEventListener('click', function () {
+
+            body.classList.toggle("xe-visible-comments");
         });
     }
+}
 
 
-    // Toggle comments
-    function xeToggleComment() {
+/**
+ * 
+ * Back to top button.
+ * 
+*/
 
-        var body = document.body;
-        var commentToggleBtn = document.getElementById("xe-comment-toggle-btn");
+function xeBackToTopButton() {
 
-        if (commentToggleBtn !== null || undefined) {
+    var backToTopBtn = document.getElementById("xe-backtotop-button");
 
-            commentToggleBtn.addEventListener('click', function() {
+    window.addEventListener('scroll', function () {
 
-                body.classList.toggle("xe-visible-comments");
+        var yAxisHeight = window.pageYOffset;
+
+        if (backToTopBtn != null || backToTopBtn != undefined) {
+
+            if (yAxisHeight > 600) {
+
+                backToTopBtn.classList.add("visible");
+
+            } else {
+
+                backToTopBtn.classList.remove("visible");
+            }
+
+            backToTopBtn.addEventListener('click', function () {
+
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            });
+        }
+    });
+}
+
+
+/**
+ * 
+ * Canvas navigation.
+ * 
+*/
+
+function xeCanvasNavigationSubMenuToggleBtn() {
+
+    var mobileCanvas = document.getElementById("xe-mobile-canvas");
+    var mobileCanvasNavChildren = document.querySelectorAll("#xe-mobile-canvas .xe-offcanvas-navigation li.menu-item-has-children");
+    var subMenuTooglePlusIconClass = "ti-plus";
+    var subMenuToogleMinusIconClass = "ti-minus";
+    var appendSubMenuToggleBtn = '<button class="xe-button xe-false-button xe-toggle-canvassubmenu"><i class="ti-plus"></button>';
+
+    if (mobileCanvas !== null || undefined) {
+
+        if (mobileCanvasNavChildren !== null || undefined) {
+
+            mobileCanvasNavChildren.forEach(function (child) {
+
+                child.insertAdjacentHTML('beforeend', appendSubMenuToggleBtn);
             });
         }
     }
 
+    // Attribute xe-submenu-style="" | options = default | revealing
+    var mobileCanvasSubMenuStyleEle = document.querySelector("#xe-mobile-canvas .is-offcanvas-menu");
 
-    // Back to top button 
-    function xeBackToTopButton() {
+    if (mobileCanvasSubMenuStyleEle !== null || undefined) {
 
-        var backToTopBtn = document.getElementById("xe-backtotop-button");
+        var mobileCanvasSubMenuStyleEleAttribute = mobileCanvasSubMenuStyleEle.getAttribute('xe-submenu-style');
 
-        window.addEventListener('scroll', function() {
+        if (mobileCanvasSubMenuStyleEleAttribute == "revealing") {
 
-            var yAxisHeight = window.pageYOffset;
+            var mobileCanvasSubMenuBackButton = '<li class="goback"><button class="xe-button xe-false-button xe-canvassubmenu-goback-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 13v7l-8-8 8-8v7h8v2z" fill="rgba(105,105,105,1)"/></svg> <span class="text">Go Back</span></button></li>';
+            var mobileCanvasSubMenuEle = document.querySelectorAll("#xe-mobile-canvas .xe-offcanvas-navigation .sub-menu");
 
-            if (backToTopBtn != null || backToTopBtn != undefined) {
+            if (mobileCanvasSubMenuEle !== null || undefined) {
 
-                if (yAxisHeight > 600) {
+                mobileCanvasSubMenuEle.forEach(function (childEle) {
 
-                    backToTopBtn.classList.add("visible");
+                    childEle.insertAdjacentHTML('afterbegin', mobileCanvasSubMenuBackButton);
+                });
+            }
 
-                } else {
+            var mobileCanvasSubMenuBackButtonEle = document.querySelectorAll("#xe-mobile-canvas .xe-offcanvas-navigation .sub-menu .xe-canvassubmenu-goback-button");
 
-                    backToTopBtn.classList.remove("visible");
-                }
+            if (mobileCanvasSubMenuBackButtonEle !== null || undefined) {
 
-                backToTopBtn.addEventListener('click', function() {
+                mobileCanvasSubMenuBackButtonEle.forEach(function (backBtn) {
 
-                    window.scrollTo({
-                        top: 0,
-                        left: 0,
-                        behavior: 'smooth'
+                    backBtn.addEventListener('click', function () {
+
+                        this.parentElement.parentElement.parentElement.classList.remove("xe-submenu-open");
                     });
                 });
             }
-        });
+        }
     }
 
-    // Append toggle button in mobile canvas navigation dropdown
-    function xeCanvasNavigationSubMenuToggleBtn() {
+    // Canvas submenu toggle button
+    var mobileCanvasSubMenuToggleBtn = document.querySelectorAll("#xe-mobile-canvas .xe-offcanvas-navigation .menu-item-has-children .xe-toggle-canvassubmenu");
 
-        var mobileCanvas = document.getElementById("xe-mobile-canvas");
-        var mobileCanvasNavChildren = document.querySelectorAll("#xe-mobile-canvas .xe-offcanvas-navigation li.menu-item-has-children");
-        var subMenuTooglePlusIconClass = "ti-plus";
-        var subMenuToogleMinusIconClass = "ti-minus";
-        var appendSubMenuToggleBtn = '<button class="xe-button xe-false-button xe-toggle-canvassubmenu"><i class="ti-plus"></button>';
+    if (mobileCanvasSubMenuToggleBtn !== null || undefined) {
 
-        if (mobileCanvas !== null || undefined) {
+        mobileCanvasSubMenuToggleBtn.forEach(function (toggleBtn) {
 
-            if (mobileCanvasNavChildren !== null || undefined) {
+            toggleBtn.addEventListener('click', function () {
 
-                mobileCanvasNavChildren.forEach(function(child) {
+                this.classList.toggle("active");
+                this.parentElement.classList.toggle("xe-submenu-open");
 
-                    child.insertAdjacentHTML('beforeend', appendSubMenuToggleBtn);
-                });
-            }
-        }
+                if (mobileCanvasSubMenuStyleEleAttribute !== "revealing") {
 
-        // Attribute xe-submenu-style="" | options = default | revealing
-        var mobileCanvasSubMenuStyleEle = document.querySelector("#xe-mobile-canvas .is-offcanvas-menu");
+                    // Toggle plus/minus icon.
+                    if (this.classList.contains("active")) {
 
-        if (mobileCanvasSubMenuStyleEle !== null || undefined) {
+                        this.querySelector("i").classList.remove(subMenuTooglePlusIconClass);
+                        this.querySelector("i").classList.add(subMenuToogleMinusIconClass);
 
-            var mobileCanvasSubMenuStyleEleAttribute = mobileCanvasSubMenuStyleEle.getAttribute('xe-submenu-style');
+                    } else {
 
-            if (mobileCanvasSubMenuStyleEleAttribute == "revealing") {
-
-                var mobileCanvasSubMenuBackButton = '<li class="goback"><button class="xe-button xe-false-button xe-canvassubmenu-goback-button"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 13v7l-8-8 8-8v7h8v2z" fill="rgba(105,105,105,1)"/></svg> <span class="text">Go Back</span></button></li>';
-                var mobileCanvasSubMenuEle = document.querySelectorAll("#xe-mobile-canvas .xe-offcanvas-navigation .sub-menu");
-
-                if (mobileCanvasSubMenuEle !== null || undefined) {
-
-                    mobileCanvasSubMenuEle.forEach(function(childEle) {
-
-                        childEle.insertAdjacentHTML('afterbegin', mobileCanvasSubMenuBackButton);
-                    });
-                }
-
-                var mobileCanvasSubMenuBackButtonEle = document.querySelectorAll("#xe-mobile-canvas .xe-offcanvas-navigation .sub-menu .xe-canvassubmenu-goback-button");
-
-                if (mobileCanvasSubMenuBackButtonEle !== null || undefined) {
-
-                    mobileCanvasSubMenuBackButtonEle.forEach(function(backBtn) {
-
-                        backBtn.addEventListener('click', function() {
-
-                            this.parentElement.parentElement.parentElement.classList.remove("xe-submenu-open");
-                        });
-                    });
-                }
-            }
-        }
-
-        // Canvas submenu toggle button
-        var mobileCanvasSubMenuToggleBtn = document.querySelectorAll("#xe-mobile-canvas .xe-offcanvas-navigation .menu-item-has-children .xe-toggle-canvassubmenu");
-
-        if (mobileCanvasSubMenuToggleBtn !== null || undefined) {
-
-            mobileCanvasSubMenuToggleBtn.forEach(function(toggleBtn) {
-
-                toggleBtn.addEventListener('click', function() {
-
-                    this.classList.toggle("active");
-                    this.parentElement.classList.toggle("xe-submenu-open");
-
-                    if (mobileCanvasSubMenuStyleEleAttribute !== "revealing") {
-
-                        // Toggle plus/minus icon.
-                        if (this.classList.contains("active")) {
-
-                            this.querySelector("i").classList.remove(subMenuTooglePlusIconClass);
-                            this.querySelector("i").classList.add(subMenuToogleMinusIconClass);
-
-                        } else {
-
-                            this.querySelector("i").classList.remove(subMenuToogleMinusIconClass);
-                            this.querySelector("i").classList.add(subMenuTooglePlusIconClass);
-                        }
+                        this.querySelector("i").classList.remove(subMenuToogleMinusIconClass);
+                        this.querySelector("i").classList.add(subMenuTooglePlusIconClass);
                     }
-                });
+                }
             });
-        }
+        });
     }
+}
 
-    // tab
-    //function xeTabs() {
-    //	var bindAllTabs = function () {
-    //		var tabElements = document.querySelectorAll("[xe-data-tab]");
+document.addEventListener("DOMContentLoaded", function () {
 
-    //		for (var i = 0; i < tabElements.length; i++) {
-    //			tabElements[i].addEventListener("click", change, false);
-    //		}
-    //	};
-
-    //	var clear = function () {
-    //		var tabElements = document.querySelectorAll("[xe-data-tab]");
-
-    //		for (var i = 0; i < tabElements.length; i++) {
-    //			tabElements[i].classList.remove("active");
-    //			var id = tabElements[i].getAttribute("xe-data-tab");
-    //			document.getElementById(id).classList.remove("active");
-    //		}
-    //	};
-
-    //	var change = function (e) {
-    //		clear();
-    //		e.preventDefault();
-    //		e.target.classList.add("active");
-    //		var id = e.currentTarget.getAttribute("xe-data-tab");
-    //		document.getElementById(id).classList.add("active");
-    //	};
-
-    //	bindAllTabs();
-    //}
-
-
-    // tab hidden nav items
-    //function xeTabHiddenItems() {
-
-    //	var hiddenTabNavToggleEle = document.querySelectorAll(".toggle-hidden-tabnav-items");
-    //	var hiddenTabNavItemsEle = document.querySelectorAll(".xe-hidden-tabnavs");
-
-    //	[].forEach.call(hiddenTabNavToggleEle, function (item) {
-    //		item.addEventListener("click", function (e) {
-    //			e.preventDefault();
-    //			var parentNav = item.parentNode;
-    //			var parentNavChildren = parentNav.children;
-    //			for (var i = 0; i < parentNavChildren.length; i++) {
-    //				if (
-    //					parentNavChildren[i].classList.contains(
-    //						"xe-hidden-tabnavs"
-    //					)
-    //				) {
-    //					parentNavChildren[i].classList.toggle("visible");
-    //				}
-    //			}
-    //		});
-    //	});
-    //}
-
-    // swiper
-    //function initSwiper() {
-    //	var swiper = new Swiper("#product-block-1-demo-carosuel", {
-    //		direction: "horizontal",
-    //		effect: "slide",
-    //		spaceBetween: 30,
-    //		slidesPerView: 4,
-    //		loopedSlides: 4,
-    //		loop: true,
-    //		freeMode: false,
-    //		lazy: true,
-    //		speed: 1000,
-    //		breakpoints: {
-    //			1024: {
-    //				slidesPerView: 4,
-    //			},
-    //			991: {
-    //				slidesPerView: 3,
-    //			},
-    //			768: {
-    //				slidesPerView: 3,
-    //			},
-    //			575: {
-    //				slidesPerView: 2,
-    //				spaceBetween: 10,
-    //			},
-    //			0: {
-    //				slidesPerView: 2,
-    //				spaceBetween: 10,
-    //			},
-    //		},
-    //		pagination: {
-    //			el: ".swiper-pagination",
-    //		},
-    //		navigation: {
-    //			nextEl: ".swiper-button-next",
-    //			prevEl: ".swiper-button-prev",
-    //		},
-    //	});
-
-    //	var swiper = new Swiper("#product-block-2-demo-carosuel", {
-    //		direction: "horizontal",
-    //		effect: "slide",
-    //		spaceBetween: 0,
-    //		slidesPerView: 1,
-    //		loopedSlides: 1,
-    //		loop: true,
-    //		freeMode: false,
-    //		lazy: true,
-    //		speed: 1000,
-    //		pagination: {
-    //			el: ".swiper-pagination",
-    //		},
-    //		navigation: {
-    //			nextEl: ".swiper-button-next",
-    //			prevEl: ".swiper-button-prev",
-    //		},
-    //	});
-    //}
-
+    xeToolTip();
+    xeToggleComment();
+    xeBackToTopButton();
+    xeCanvasNavigationSubMenuToggleBtn();
 });
