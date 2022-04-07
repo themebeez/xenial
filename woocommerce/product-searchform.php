2dfb8a7820ebffb7c9c5bb8116ab32acd1359d11
h1dd3n_sn1p3r <struggleage@gmail.com>
2022-04-07 18:00:45 +0545

    Tweak: CSS variables & WooCommerce global end wrapper
    Tweak: CSS variables & WooCommerce global end wrapper

--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -23,6 +23,8 @@ $xenial_search_input_placeholder = xenial_get_option( 'header_search_placeholder
 <form role="search" method="get" class="woocommerce-product-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label class="screen-reader-text" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>"><?php esc_html_e( 'Search for:', 'xenial' ); ?></label>
 	<input type="search" id="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>" class="search-field" placeholder="<?php echo esc_attr( $xenial_search_input_placeholder ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
-	<button type="submit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'xenial' ); ?>"><?php echo esc_html_x( 'Search', 'submit button', 'xenial' ); ?></button>
+	<button class ="xe-button xe-false-button xe-search-form-submit-btn" type ="submit">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M11 2c4.968 0 9 4.032 9 9s-4.032 9-9 9-9-4.032-9-9 4.032-9 9-9zm0 16c3.867 0 7-3.133 7-7 0-3.868-3.133-7-7-7-3.868 0-7 3.132-7 7 0 3.867 3.132 7 7 7zm8.485.071l2.829 2.828-1.415 1.415-2.828-2.829 1.414-1.414z"/></svg>
+    </button>
 	<input type="hidden" name="post_type" value="product" />
 </form>
